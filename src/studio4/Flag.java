5529e65d654bd8d3528fd8a03385bf57cd3b2876
Owner <Owner@DESKTOP-VVHG0CU.dhcp.wustl.edu>
2022-10-12 15:46:45 -0500

    1234

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,16 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.line(0.3, 0.1, 0.8, 0.8);
+		StdDraw.line(0.1, 0.3, 0.8, 0.8);
+		StdDraw.line(0.3, 0.1, 0.1, 0.3);
+		StdDraw.ellipse(0.87, 0.87, 0.1, 0.1);
 		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.line(0.9, 0.3, 0.2, 0.8);
+		StdDraw.line(0.7, 0.1, 0.2, 0.8);
+		StdDraw.line(0.9, 0.3, 0.7, 0.1);
+		StdDraw.ellipse(0.13, 0.87, 0.1, 0.1);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,22 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int R = in.nextInt();
+		int G = in.nextInt();
+		int B = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x1 = in.nextDouble();
+		double y1 = in.nextDouble();
+		double r1 = in.nextDouble();
+		double r2 = in.nextDouble();
+		if (shape.equals("ellipse")) {
+			StdDraw.setPenColor(R,G,B);
+			StdDraw.ellipse(x1,y1,r1,r2);
+			
+			if (filled) {
+				StdDraw.filledEllipse(x1, y1, r1, r2);
+			}
+		}
 	}
 }
